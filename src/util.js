cb028892b066dad6955bcfa83e5a218fa93b6947
석진 <37528272+seokjin44@users.noreply.github.com>
2023-10-10 10:11:09 +0900

    [수정] KP단위 텍스트변환 함수 추가

--- src/util.js
+++ src/util.js
@@ -5,3 +5,16 @@ export const dateFormat = ( date ) => {
   
     return `${yyyy}-${mm}-${dd}`;
 }
+
+export const convertToCustomFormat = (num) => {
+    if (typeof num !== 'number' || num < 1000) {
+        return 'Input should be a number greater than or equal to 1000';
+    }
+
+    let thousandPart = Math.floor(num / 1000);
+    let remainderPart = num % 1000;
+    
+    let formattedRemainder = remainderPart.toString().padStart(3, '0'); // 0을 채워서 3자리 문자열로 만듭니다.
+    
+    return `${thousandPart}k${formattedRemainder}`;
+}
