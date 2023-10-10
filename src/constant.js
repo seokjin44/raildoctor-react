d7fe935e171df266f9099016aa1c931711caf9bb
석진 <37528272+seokjin44@users.noreply.github.com>
2023-10-10 10:10:46 +0900

    [추가] 상하선 상수등록

--- src/constant.js
+++ src/constant.js
@@ -2,6 +2,10 @@ export const ROUTE_INCHEON_1 = 1;
 export const ROUTE_SEOUL_2 = 2;
 export const ROUTE_OSONG_TEST = 3;
 export const ROUTE_KTX_EXPRESS = 4;
+
+export const UP_TRACK = 1;
+export const DOWN_TRACK = 2;
+
 export const RANGEPICKERSTYLE = {
   height:"35px",
   fontFamily : 'NEO_R'
